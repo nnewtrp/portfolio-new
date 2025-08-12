d321d00782e755e719b57d3b31b7069a004bda1d
nnewtrp <newnack2013@gmail.com>
2025-08-12 22:50:49 +0700

    Update Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,60 @@
-import Image from "next/image";
+import { Box } from "@mui/material";
 import styles from "./page.module.css";
+
+var containerStyle = { flexGrow: 1, height: 600, overflow: 'hidden', display: 'flex' }
+var reverseStyle = { flexDirection: 'row-reverse', backgroundColor: 'black' }
+var imageContainerStyle = { display: { xs: 'none', md: 'block' }, overflow: 'hidden' }
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <>
+      {/* Main */}
+      <Box sx={containerStyle}>
+        <Box sx={imageContainerStyle}>
+          <img
+            src="/home/HomeImage.jpg"
+            alt="Picture of the author"
+            className={styles.mainImage}
+          />
+        </Box>
+        <div className={styles.mainContainer} style={{ textAlign: 'center' }}>
+          <h1 style={{ fontSize: 64 }}>Teerapat Satitporn</h1>
+          <p style={{ fontSize: 30, marginTop: 10 }}>Web Developer</p>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+      </Box>
+
+      {/* Education */}
+      <Box sx={[containerStyle, reverseStyle]}>
+        <Box sx={imageContainerStyle}>
+          <img
+            src="/home/HomeImage.jpg"
+            alt="Picture of the author"
+            className={styles.mainImage}
           />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+        </Box>
+        <div className={styles.mainContainer} style={{ color: 'white' }}>
+          <h1 className={styles.title}>Education</h1>
+        </div>
+      </Box>
+
+      {/* Work Experience */}
+      <Box sx={containerStyle}>
+        <Box sx={imageContainerStyle}>
+          <img
+            src="/home/HomeImage.jpg"
+            alt="Picture of the author"
+            className={styles.mainImage}
           />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        </Box>
+        <div className={styles.mainContainer}>
+          <h1 className={styles.title}>Work Experience</h1>
+        </div>
+      </Box>
+
+      {/* Programming Skill */}
+
+      {/* Contact */}
+      
+    </>
   );
 }
