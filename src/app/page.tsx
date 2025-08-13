67af176e737a39845138456d1ad5c929ccec7a98
nnewtrp <newnack2013@gmail.com>
2025-08-14 00:34:53 +0700

    Update Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,49 +12,79 @@ export default function Home() {
       <Box sx={containerStyle}>
         <Box sx={imageContainerStyle}>
           <img
-            src="/home/HomeImage.jpg"
+            src="/home/me1.jpg"
             alt="Picture of the author"
             className={styles.mainImage}
           />
         </Box>
         <div className={styles.mainContainer} style={{ textAlign: 'center' }}>
           <h1 style={{ fontSize: 64 }}>Teerapat Satitporn</h1>
-          <p style={{ fontSize: 30, marginTop: 10 }}>Web Developer</p>
+          <p style={{ fontSize: 30, marginTop: 10 }}>Software Developer</p>
         </div>
       </Box>
 
-      {/* Education */}
+      {/* About Me */}
       <Box sx={[containerStyle, reverseStyle]}>
         <Box sx={imageContainerStyle}>
           <img
-            src="/home/HomeImage.jpg"
+            src="/home/me2.jpg"
             alt="Picture of the author"
             className={styles.mainImage}
           />
         </Box>
         <div className={styles.mainContainer} style={{ color: 'white' }}>
-          <h1 className={styles.title}>Education</h1>
+          <h1 className={styles.title}>About Me</h1>
         </div>
       </Box>
 
-      {/* Work Experience */}
+      {/* Education */}
       <Box sx={containerStyle}>
         <Box sx={imageContainerStyle}>
           <img
-            src="/home/HomeImage.jpg"
+            src="/home/study.jpg"
             alt="Picture of the author"
             className={styles.mainImage}
           />
         </Box>
         <div className={styles.mainContainer}>
+          <h1 className={styles.title}>Education</h1>
+        </div>
+      </Box>
+
+      {/* Work Experience */}
+      <Box sx={[containerStyle, reverseStyle]}>
+        <Box sx={imageContainerStyle}>
+          <img
+            src="/home/work.jpg"
+            alt="Picture of the author"
+            className={styles.mainImage}
+          />
+        </Box>
+        <div className={styles.mainContainer} style={{ color: 'white' }}>
           <h1 className={styles.title}>Work Experience</h1>
         </div>
       </Box>
 
-      {/* Programming Skill */}
+      {/* Skills */}
+      <Box sx={containerStyle}>
+        <div className={styles.mainContainer} style={{ textAlign: 'center' }}>
+          <h1 className={styles.title}>Skills & Abilities</h1>
+        </div>
+      </Box>
 
       {/* Contact */}
-      
+      <Box sx={[containerStyle, reverseStyle]}>
+        <Box sx={imageContainerStyle}>
+          <img
+            src="/home/me3.jpg"
+            alt="Picture of the author"
+            className={styles.mainImage}
+          />
+        </Box>
+        <div className={styles.mainContainer} style={{ color: 'white' }}>
+          <h1 className={styles.title}>Contact</h1>
+        </div>
+      </Box>
     </>
   );
 }
