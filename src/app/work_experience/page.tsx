46e89364ec5f66899531b3f85eb7402441c58bda
nnewtrp <newnack2013@gmail.com>
2025-08-21 00:23:23 +0700

    Update work experience page

--- src/app/work_experience/page.tsx
+++ src/app/work_experience/page.tsx
@@ -1,13 +1,26 @@
 'use client'
-import { Box, Button } from "@mui/material"
 import styles from "./page.module.css"
 import jobList from "./jobs.json"
-
-// Styles
+import moment from "moment"
 
 export default function Education() {
   return (
-    <>
-    </>
+    <div className={styles.mainContainer}>
+      {/* Title */}
+      <h1 className={styles.title}>Work Experience</h1>
+
+      {/* Work Experience Timeline */}
+      {jobList.map((item) => {return (
+        <div key={item.short_name} className={styles.itemContainer}>
+          <div>
+            <h3 className={styles.companyName}>{item.company}</h3>
+            <p className={styles.position}>{item.position}</p>
+            <p className={styles.period}>
+              {moment(item.startDate).format("MMM YYYY")} - {item.endDate ? moment(item.endDate).format("MMM YYYY") : "Present"}
+            </p>
+          </div>
+        </div>
+      )})}
+    </div>
   )
 }
