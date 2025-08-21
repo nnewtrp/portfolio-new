9606143cd5029d46529f74516766fbbdd4602620
Teerapat Satitporn <newnack2013@gmail.com>
2025-08-21 17:36:53 +0700

    Create projectList and add headers in project page

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,12 +1,20 @@
 'use client'
 import { Divider } from "@mui/material"
 import styles from "./page.module.css"
+import projectList from "./projects.json"
 
 export default function ProjectPage() {
   return (
-    <div className={styles.mainContainer}>
-      {/* Title */}
-      <h1 className={styles.title}>Personal Projects</h1>
-    </div>
+    <>
+      {projectList.map((header) => (
+        <div key={header.title} className={styles.mainContainer}>
+          {/* Title */}
+          <h1 className={styles.title}>{header.title}</h1>
+          <Divider />
+
+          {/* Items */}
+        </div>
+      ))}
+    </>
   )
 }
