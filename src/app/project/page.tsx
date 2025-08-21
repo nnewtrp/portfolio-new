b1a55eba394cc6a8360c8e89fc5ffb7dba349ad1
nnewtrp <newnack2013@gmail.com>
2025-08-21 23:28:48 +0700

    update item styles in project page

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { Divider } from "@mui/material"
+import CustomIcon from "@/components/CustomIcon"
 import styles from "./page.module.css"
 import projectList from "./projects.json"
 
@@ -13,6 +14,33 @@ export default function ProjectPage() {
           <Divider />
 
           {/* Items */}
+          <div className={styles.itemContainer}>
+            {header.items.map((item) => (
+              <div key={item.name} className={styles.item}>
+                {/* Project Name */}
+                <h1 className={styles.itemTitle}>{item.name}</h1>
+
+                <Divider sx={{ marginY: 1 }} />
+
+                {/* Project Description */}
+                <div className={styles.itemDescription}>
+                  {item.description}
+                </div>
+
+                <div style={{ flex: 1 }} />
+
+                {/* Icons */}
+                <div className={styles.itemIconContainer}>
+                  {item.github_path ? (
+                    <CustomIcon
+                      icon="GitHub"
+                      onClick={() => {window.open(process.env.NEXT_PUBLIC_GITHUB_URL + item.github_path)}}
+                    />
+                  ) : null}
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       ))}
     </>
