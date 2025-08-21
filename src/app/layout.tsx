7bb4cca1e81c4943082c912558c35125a7081ecf
nnewtrp <newnack2013@gmail.com>
2025-08-22 00:02:26 +0700

    Create Footer layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,22 +2,23 @@ import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter'
 import { Box, AppBar, Toolbar, Typography } from "@mui/material"
+import Footer from "@/components/layouts/Footer"
 import "./globals.css"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
-});
+})
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
-});
+})
 
 export const metadata: Metadata = {
   title: "Teerapat Satitporn - Welcome to my portfolio website",
   description: "Teerapat Satitporn Portfolio Website",
-};
+}
 
 export default function RootLayout({
   children,
@@ -28,6 +29,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <AppRouterCacheProvider>
+          {/* Nav Bar */}
           <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static" color="info" sx={{ backgroundColor: 'black' }}>
               <Toolbar>
@@ -37,7 +39,13 @@ export default function RootLayout({
               </Toolbar>
             </AppBar>
           </Box>
+
+          {/* Body */}
           {children}
+
+          {/* Footer */}
+          <div style={{ flex: 1 }} />
+          <Footer />
         </AppRouterCacheProvider>
       </body>
     </html>
---
+++ src/components/layouts/Footer.tsx
@@ -0,0 +1,22 @@
+'use client'
+import { Divider } from "@mui/material"
+import CustomIcon from "../CustomIcon"
+import styles from "./layout.module.css"
+
+const iconColor = "white"
+
+export default function Footer() {
+  return (
+    <>
+      <Divider />
+      <div className={styles.bottomBarContainer}>
+        <div style={{ flex: 1 }} />
+        <CustomIcon icon="Email" color={iconColor} />
+        <CustomIcon icon="Instagram" color={iconColor} />
+        <CustomIcon icon="Facebook" color={iconColor} />
+        <CustomIcon icon="GitHub" color={iconColor} />
+        <CustomIcon icon="LinkedIn" color={iconColor} />
+      </div>
+    </>
+  )
+}
