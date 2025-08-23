729712fd01f44d05233d8e9d26078b574e2e6539
nnewtrp <newnack2013@gmail.com>
2025-08-24 01:59:59 +0700

    add menuList in NavBar component

--- src/components/layouts/NavBar.tsx
+++ src/components/layouts/NavBar.tsx
@@ -1,8 +1,20 @@
 'use client'
 import { useState } from "react";
 import { AppBar, Toolbar, Typography, Menu, MenuItem, Button } from "@mui/material"
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import { useRouter } from 'next/navigation'
+
+const menuList = [
+  { title: "Home", path: "/" },
+  { title: "Education", path: "/education" },
+  { title: "Work Experiance", path: "/experiance" },
+  { title: "Programming Skills", path: "/programming" },
+  { title: "Personal Projects", path: "/project" },
+  { title: "Other Activities", path: "/activity" }
+]
 
 export default function NavBar() {
+  const router = useRouter()
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
 
   const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
@@ -10,8 +22,13 @@ export default function NavBar() {
   }
 
   const handleClose = () => {
-    setAnchorEl(null);
-  };
+    setAnchorEl(null)
+  }
+
+  const handleMenuOnClick = (path: string) => {
+    router.push(path)
+    handleClose()
+  }
 
   return (
     <AppBar sx={{ backgroundColor: 'black' }}>
@@ -22,8 +39,8 @@ export default function NavBar() {
         </Typography>
 
         {/* Menu */}
-        <Button onClick={handleMenu} sx={{ color: "white" }}>
-          <b>Menu</b>
+        <Button onClick={handleMenu} sx={{ color: "white", fontWeight: "bold" }}>
+          Menu <ArrowDropDownIcon sx={{ rotate: anchorEl ? "180deg" : null }} />
         </Button>
         <Menu
           id="menu-appbar"
@@ -37,11 +54,13 @@ export default function NavBar() {
             vertical: 'top',
             horizontal: 'right',
           }}
+          sx={{ mt: "35px" }}
           open={Boolean(anchorEl)}
           onClose={handleClose}
         >
-          <MenuItem onClick={handleClose}>Profile</MenuItem>
-          <MenuItem onClick={handleClose}>My account</MenuItem>
+          {menuList.map((menu) => (
+            <MenuItem key={menu.title} onClick={() => handleMenuOnClick(menu.path)}>{menu.title}</MenuItem>
+          ))}
         </Menu>
       </Toolbar>
     </AppBar>
